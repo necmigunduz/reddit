5da004ad9d069b84573565fc71c6187c49a6de77
patrick-angelos <angelosp07@gmail.com>
2021-02-05 16:16:49 +0200

    Change ruby verision

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_205_133_749) do
-  create_table 'comments', force: :cascade do |t|
-    t.text 'body'
-    t.integer 'user_id'
-    t.integer 'post_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2021_02_05_133749) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.integer 'user_id'
-    t.string 'url'
-    t.string 'title'
-    t.text 'body'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "url"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'user_name'
-    t.string 'email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end
